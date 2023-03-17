798d60b0db744d3255d287320d5e13961399968f
Tom Moyer <tom.moyer@uncc.edu>
2023-03-17 18:57:11 -0400

    first potentially working version of the script

--- watson2sf.py
+++ watson2sf.py
@@ -2,16 +2,90 @@
 
 import csv
 import click
+import re
+import math
+
+from string import Template
+from datetime import datetime, timedelta, date
+
+
+entryTemplate = Template(('[\\"$name\\",\\"$caseNumber\\",\\"$minutes\\",'
+                          '\\"$date\\",\\"9:00 AM\\",\\"$note\\"]'))
+projectCaseFormat = re.compile(r"^sf-([0-9]{8})$")
+tagCaseFormat = re.compile(r"^[0-9]{8}$")
+roundingDelta = timedelta(minutes=15)
+
+
+def round_dt(dt, delta):
+    return datetime.min + math.ceil((dt - datetime.min) / delta) * delta
+
+
+def extractCaseNumber(project, tags):
+    caseNumber = None
+
+    if projectCaseFormat.match(project):
+        caseNumber = projectCaseFormat.match(project).group(1)
+    elif tagCaseFormat.match(tags):
+        caseNumber = tags
+    else:
+        print(f"No valid case number found for {project} or {tags}")
+        while not caseNumber:
+            caseNumberInput = input("Enter case number: ")
+            if tagCaseFormat.match(caseNumberInput):
+                caseNumber = caseNumberInput
+            else:
+                print(f"{caseNumberInput} isn't a valid SalesForce case")
+
+    return caseNumber
+
+
+def extractMinutesWorked(startTimeStr, stopTimeStr):
+    startTime = datetime.strptime(startTimeStr, '%Y-%m-%d %H:%M:%S')
+    roundedStart = round_dt(startTime, roundingDelta)
+    stopTime = datetime.strptime(stopTimeStr, '%Y-%m-%d %H:%M:%S')
+    roundedStop = round_dt(stopTime, roundingDelta)
+
+    minutes = int((roundedStop - roundedStart).total_seconds() / 60)
+
+    return minutes
+
+
+def extractDate(dateString):
+    dateObject = datetime.strptime(dateString, '%Y-%m-%d %H:%M:%S')
+
+    return dateObject.strftime('%Y/%m/%d')
+
+
+def generateSeleniumScript(jsonOutput):
+    with open("new-timecards.side", "r") as timecards:
+        lines = timecards.readlines()
+        filename = f"timecards-{date.today().strftime('%Y-%m-%d')}.side"
+    with open(filename, "w") as timecards:
+        for line in lines:
+            timecards.write(re.sub(r'^#TIMECARDS_JSON#$', jsonOutput, line))
 
 
 @click.command()
 @click.option('-n', '--name', help='Full SF username')
-@click.option('-d', '--date',
-              help='Date to create input for, defaults to today')
-def cli(name, date):
-    with open('timecards.2023-03-14.csv', 'r', ) as csvInput:
-        reader = csv.DictReader(csvInput)
-        for row in reader:
-            print(f"{row['project']} : {row['start']} - {row['stop']}")
+@click.argument('file', type=click.File('r'))
+def cli(name, file):
+    if not name:
+        name = input("Enter SalesForce display name: ")
 
+    jsonOutput = '"['
 
+    reader = csv.DictReader(file)
+    for row in reader:
+        caseNumber = extractCaseNumber(row['project'], row['tags'])
+        minutes = extractMinutesWorked(row['start'], row['stop'])
+        workDate = extractDate(row['start'])
+        jsonOutput += entryTemplate.substitute(name=name,
+                                               caseNumber=caseNumber,
+                                               minutes=minutes,
+                                               date=workDate,
+                                               note=row['note'])
+
+    jsonOutput += ']"'
+
+    print(jsonOutput)
+    generateSeleniumScript(jsonOutput)
